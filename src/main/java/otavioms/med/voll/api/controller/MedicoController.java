ac819a3e9b8b9adaa17b6bc0f2c1a512d1974d40
otavioms <otaviioms@gmail.com>
2024-02-05 22:36:21 +0000

    Criando Interfaces Repository

--- src/main/java/otavioms/med/voll/api/controller/MedicoController.java
+++ src/main/java/otavioms/med/voll/api/controller/MedicoController.java
@@ -1,16 +1,23 @@
 package otavioms.med.voll.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import otavioms.med.voll.api.endereco.Endereco;
 import otavioms.med.voll.api.medico.DadosCadastroMedico;
+import otavioms.med.voll.api.medico.Medico;
+import otavioms.med.voll.api.medico.MedicoRepository;
 
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
+
     //Serve para informar que quando chegar uma requisão POST tem que chamar este método
     @PostMapping          //Serve para receber dados do corpo da requisição
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados);
+        repository.save(new Medico(dados));
     }
 
 }
--- src/main/java/otavioms/med/voll/api/endereco/DadosEndereco.java
+++ src/main/java/otavioms/med/voll/api/endereco/DadosEndereco.java
@@ -1,4 +1,4 @@
 package otavioms.med.voll.api.endereco;
 
-public record DadosEndereco(String logradouro, String bairro, String cep, String uf, String numero, String complemento) {
+public record DadosEndereco(String logradouro, String bairro, String cep, String uf, String numero, String complemento, String cidade) {
 }
